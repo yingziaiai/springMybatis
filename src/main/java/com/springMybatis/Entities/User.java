d96995d84942a89894ab54a0b4a6f03e8d120c5b
yingziaiai <06fuping@163.com>
2016-12-07 13:58:44 +0800

    adjust the format

--- src/main/java/com/springMybatis/Entities/User.java
+++ src/main/java/com/springMybatis/Entities/User.java
@@ -1,39 +1,46 @@
 package com.springMybatis.Entities;
 
 public class User {
-private int id;
-private String name;
-private int age;
-private String sex;
+	private int id;
+	private String name;
+	private int age;
+	private String sex;
 
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public int getAge() {
-	return age;
-}
-public void setAge(int age) {
-	this.age = age;
-}
-public String getSex() {
-	return sex;
-}
-public void setSex(String sex) {
-	this.sex = sex;
-}
+	public int getId() {
+		return id;
+	}
 
-public void toString(User user){
-	System.out.println("用户id：" + id + "\n用户名"+ name + "\n用户年龄"+ age +  "\n用户性别" + sex);
-}
+	public void setId(int id) {
+		this.id = id;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public void toString(User user) {
+		System.out.println("用户id：" + id + "\n用户名" + name + "\n用户年龄" + age
+				+ "\n用户性别" + sex);
+	}
 
 }
